14bfe7ed47ccd8d5d535f0e34507b99db574d05a
Peter Benko <Peter Benko@DESKTOP-6L6GM63>
2020-01-17 12:20:42 +0000

    CreatingFormsExcersize4

--- WebAppVidly/Controllers/MoviesController.cs
+++ WebAppVidly/Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using WebAppVidly.Models;
+using WebAppVidly.ViewModel;
 
 namespace WebAppVidly.Controllers
 {
@@ -26,7 +27,6 @@ namespace WebAppVidly.Controllers
             return View(movies);
         }
 
-
         public ActionResult Details(int id)
         {
             var movies = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
@@ -35,6 +35,57 @@ namespace WebAppVidly.Controllers
                 return HttpNotFound();
 
             return View(movies);
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genre.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genre.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+
         }
     }
 }
